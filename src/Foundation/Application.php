c6c60bbf9bc5ab2999024659888fc625b46b3ebf
Puleeno Nguyen <puleeno@gmail.com>
2024-04-14 09:37:25 +0700

    Create new Application class prepare to refactor

---
+++ src/Foundation/Application.php
@@ -0,0 +1,8 @@
+<?php
+namespace Quagga\Quagga\Foundation;
+
+use Illuminate\Container\Container;
+use Quagga\Contracts\Foundation\ApplicationContract;
+
+class Application extends Container implements ApplicationContract {
+}
